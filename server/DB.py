1e9688622b39e8f3d7dd86f727167a93d55949fa
triest <you@example.com>
2019-01-05 01:14:46 +0300

    get tracsaction fpor last 3 days

--- server/DB.py
+++ server/DB.py
@@ -2,21 +2,19 @@ from os import getenv
 import pymysql
 import json
 from django.core.serializers import json
+import requests
 
 
 class DB:
     cursor = ""
-    server = getenv("MACHINE-VOIV7EH\SQLEXPRESS");
-    database = getenv("transfers")
-    username = getenv("MACHINE-VOIV7EH\\triest")
-    password = getenv("")
+
 
     def conect(self):
         db = pymysql.connect("localhost", "root", "", "python")
         cur = db.cursor()
+        return cur
 
     def test(self):
-        print("func")
         connection = pymysql.connect("localhost", "root", "", "python", charset='utf8mb4',
                                      cursorclass=pymysql.cursors.DictCursor)
         try:
@@ -36,21 +34,49 @@ class DB:
                                      cursorclass=pymysql.cursors.DictCursor)
         try:
             with connection.cursor() as cursor:
-                # Read a single record
+                # insert in databese
                 sql = "INSERT INTO `transfers`( `method`, `date`, `account`, `amt`, `ccy`) VALUES (%s,%s,%s,%s,%s)"
                 # cursor.execute(sql, ('deposit', '2018-12-09', 'bob', 12, 'EUR',))
                 cursor.execute(sql, (method, date, account, amt, ccy))
                 result = cursor.fetchone()
                 connection.commit()
-                print(result)
         except Exception:
-            print('Это что ещё такое?')
+            print("Insert Error:")
         finally:
             # Close connection.
             connection.close()
 
     # parse json for insert in DB
     def parse(self, data):
-        print("in parse:")
         self.insert(data['method'], data['date'], data['account'], data['amt'], data['ccy'])
-  
+
+    # get course from API and convert to EURO, return EURO
+    def convert(self, ammount, ccy):  # ссн - валюта для перевода
+        r = requests.get(url='https://api.exchangeratesapi.io/latest?symbols=' + ccy)
+        r = r.json()
+        rates = r['rates'][ccy]
+        euro = ammount / rates
+        return euro
+
+    # check history operation and summ in euro prom last 3 dats
+    def calcSumInEuro(self):
+        connection = pymysql.connect("localhost", "root", "", "python", charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
+        try:
+            with connection.cursor() as cursor:
+                # get tracsactions for last 3 days
+                sql = "SELECT * FROM `transfers` WHERE `create_at` BETWEEN CURRENT_TIMESTAMP - INTERVAL '3' DAY AND CURRENT_TIMESTAMP"  # получили транзакции за последние 3 дня
+                cursor.execute(sql)
+                for row in cursor:
+                    print(row)
+        except Exception:
+            print("Insert Error:")
+        finally:
+            # Close connection.
+            connection.close()
+
+
+
+            # SELECT * FROM `transfers` WHERE create_at
+            #      BETWEEN CURRENT_TIMESTAMP - INTERVAL '3' DAY
+            #           AND CURRENT_TIMESTAMP
--- test.py
+++ test.py
@@ -2,5 +2,9 @@ from server.DB import DB
 
 db = DB();
 # db.conect();
-db.test()
-db.insert('deposit', '2018-12-09', 'bob', 12, 'EUR')
+# db.test()
+# db.insert('deposit', '2018-12-09', 'bob', 12, 'EUR')
+
+# euro=db.convert(77.679,'RUB')
+# print(euro)
+db.calcSumInEuro()
